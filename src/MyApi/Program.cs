ea401c1aa429f9f5f06e5de43dcf79a8ff385828
typshit <main.typesh@outlook.com>
2024-08-22 08:45:32 +0000

    e

--- src/MyApi/Program.cs
+++ src/MyApi/Program.cs
@@ -1,44 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args)
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/randomnumber", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
+    var randomNumber = new RandomNumber
+    {
+        Number = new Random().Next(1, 100)
+    };
+    return randomNumber;
+}
+)
+.WithName("RandomNumber")
 .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+record RandomNumber
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    public int Number { get; init; }
+};
--- src/MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ src/MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8685857c4f5dc89037a3ad2ae604b41c7992f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409a97f83b3164ddb490ebfc5f391eb1aea4eb3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
